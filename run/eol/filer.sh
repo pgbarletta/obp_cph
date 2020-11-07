7eb0cddd70bb41364d8f5a2e16bff943579d92a4
pgbarletta <pbarletta@gmail.com>
2020-11-07 15:57:23 -0300

    EOL. Getting ready for new data.

---
+++ run/eol/filer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mv pre_pdt/30ph/30p_eol.nc pdt/30ph/
+mv pre_pdt/35ph/35p_eol.nc pdt/35ph/
+mv pre_pdt/40ph/40p_eol.nc pdt/40ph/
+mv pre_pdt/45ph/45p_eol.nc pdt/45ph/
+mv pre_pdt/50ph/50p_eol.nc pdt/50ph/
+mv pre_pdt/55ph/55p_eol.nc pdt/55ph/
+mv pre_pdt/60ph/60p_eol.nc pdt/60ph/
+mv pre_pdt/65ph/65p_eol.nc pdt/65ph/
+mv pre_pdt/70ph/70p_eol.nc pdt/70ph/
+mv pre_pdt/75ph/75p_eol.nc pdt/75ph/
+
+exit 0
