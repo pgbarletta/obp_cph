1af6f8f9be4cab9cfe9f09c5476b8d4e1693b942
pgbarletta <pbarletta@gmail.com>
2020-11-14 13:38:44 -0300

    CTV RMSF analysis by pH

--- src/script_informe_ctv_rmsf.py
+++ src/script_informe_ctv_rmsf.py
@@ -2,7 +2,7 @@
 # `nb_templater` generated Python script
 # Generated from .ipynb template: ctv_rmsf.ipynb
 # www.github.com/ismailuddin/jupyter-nb-templater/
-# Generated on: 2020-10-13 09:56 
+# Generated on: 2020-11-10 20:43 
 
 import nbformat as nbf 
 import sys
@@ -18,31 +18,30 @@ cell_1=r"""
 home = "/home/pbarletta/labo/20/cph_obp/"
 dir_ctv_rmsf = string(home, "run/ctv/rmsf/")
 
-nstlim = 19500
 aa = 113
-phs = collect(30:5:75)
-idx = collect(1:10)
-pdt_steps = collect(0:1:7)
-titrable_cnt = 36
-titrable_resis = [4, 5, 7, 11, 13, 18, 20, 24, 27, 30, 33, 37, 39,
-    40, 41, 42, 48, 52, 58, 59, 64, 69, 73, 77, 78, 82, 87, 93, 94,
-    97, 99, 102, 109, 111, 114, 117];
-titrable_resnames = ["GL4", "GL4", "LYS", "HIP", "GL4", "LYS", "GL4", "AS4", "LYS",
-    "AS4", "GL4", "AS4", "GL4", "AS4", "LYS", "LYS", "GL4", "LYS",
-    "AS4", "LYS", "LYS", "LYS", "GL4", "AS4", "GL4", "LYS", "AS4",
-    "GL4", "GL4", "HIP", "LYS", "LYS", "LYS", "LYS", "LYS", "AS4"];
+nstlim = 12000
+phs = collect(2.0:.5:7.5)
+idx = collect(1:12)
+
+titrable_resis = [4, 5, 11, 13, 20, 24, 30, 33, 37, 39, 40,
+    48, 58, 73, 77, 78, 87, 93, 94, 97, 117]
+titrable_resnames = ["GL4", "GL4", "HIP", "GL4", "GL4", "AS4",
+    "AS4", "GL4", "AS4", "GL4", "AS4", "GL4", "AS4", "GL4", "AS4",
+    "GL4", "AS4", "GL4", "GL4", "HIP", "AS4"]
+titrable_cnt = length(titrable_resis)
 """.strip()
 
 cell_2=r"""
-for i in 1:10
-    global rmsf_ctv_idx = readdlm(joinpath(dir_ctv_rmsf, string(i),
-            string("rmsf_ctv_", idx[i])))[2:end, 2]
-    global rmsf_ctv_ph = readdlm(joinpath(dir_ctv_rmsf, string(phs[i], "ph"),
+for i in 1:length(phs)
+    PH = convert(Int64, phs[i] * 10)
+#     global rmsf_ctv_idx = readdlm(joinpath(dir_ctv_rmsf, string(i),
+#             string("rmsf_ctv_", idx[i])))[2:end, 2]
+    global rmsf_ctv_ph = readdlm(joinpath(dir_ctv_rmsf, string(phs[i]),
             string("rmsf_ctv_", phs[i])))[2:end, 2]
 
-    sym_rmsf_ctv_idx = Symbol("rmsf_ctv_", idx[i])
-    sym_rmsf_ctv_ph = Symbol("rmsf_ctv_", phs[i])
-    eval(:($sym_rmsf_ctv_idx = rmsf_ctv_idx))
+#     sym_rmsf_ctv_idx = Symbol("rmsf_ctv_", idx[i])
+    sym_rmsf_ctv_ph = Symbol("rmsf_ctv_", PH)
+#     eval(:($sym_rmsf_ctv_idx = rmsf_ctv_idx))
     eval(:($sym_rmsf_ctv_ph = rmsf_ctv_ph))
 end
 """.strip()
@@ -58,27 +57,24 @@ nb['cells'] = [
     nbf.v4.new_markdown_cell(cell_3),
 ]
 
-for ph in range(30, 80, 5):
+for ph in range(20, 80, 5):
+    PH = ph / 10
     celda_ph = r"""plot(collect(1:aa), rmsf_ctv_""" + str(ph) + r""",
-        title = string("RMSF - pH = ", """ + str(ph) + r"""), size = (750, 400), label = false,
-        xticks = collect(5:10:aa), yaxis = "RMSF [A]", xaxis = "Residue #")
+        title = string("RMSF - pH = ", """ + str(PH) + r"""), size = (750, 400), label = false,
+        ylims = (0, +Inf), xticks = collect(5:10:aa), yaxis = "RMSF [A]", xaxis = "Residue #")
     """.strip()
     nb['cells'].append(nbf.v4.new_code_cell(celda_ph))
 
 
-celda_titulo=r"""
-## RMSF por idx
-""".strip()
-
+celda_titulo=r"""## RMSF por idx""".strip()
 nb['cells'].append(nbf.v4.new_markdown_cell(celda_titulo))
 
-for idx in range(1, 11):
+for idx in range(1, 13):
     celda_idx=r"""plot(collect(1:aa), rmsf_ctv_""" + str(idx) + r""", 
         title = string("RMSF - idx = ", """ + str(idx) + r"""), size = (750, 400), label = false,
-        xticks = collect(5:10:aa), yaxis = "RMSF [A]", xaxis = "Residue #")
+        ylims = (0, +Inf), xticks = collect(5:10:aa), yaxis = "RMSF [A]", xaxis = "Residue #")
     """.strip()
-    nb['cells'].append(nbf.v4.new_code_cell(celda_idx))
-
+    nb['cells'].append(nbf.v4.new_markdown_cell(celda_idx))
 
 
 nb['metadata'] = {'kernelspec': {'display_name': 'Julia 1.5.0', 'language': 'julia', 'name': 'julia-1.5'}}
